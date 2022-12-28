b12b5caa3e41bf6d94e025f519e6dd81774f091f
loupdemon <azzougaghiles@hotmail.com>
2022-12-28 14:20:54 +0100

    dom and navigator

---
+++ Navigator/app.js
@@ -0,0 +1,7 @@
+console.log("salut");
+console.log(this);
+alert("faut cliquer pour lancer dab le reste JS");
+console.log(window.location);
+console.log(window.setTimeout(console.log, 1000));
+
+const a = 5;
